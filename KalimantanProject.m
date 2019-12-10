d5fe1fcd198eeb3373209f75735dfad8645526fa
Michael-Perna <michael.perna.ch@gmail.com>
2019-12-10 17:50:42 +0100

    Try my best with mapshow :)

--- KalimantanProject.m
+++ KalimantanProject.m
@@ -1,17 +1,26 @@
 % IMAGE PROCESSING FOR EARTH PROJECT, November 2019
 % Project: Palm Oil Plantation detection in Kalimatan Region 
 % Written by Leona Repnik & Michael Perna
-
+close all 
 %% Set the data directory
 directory = './Data';
 
 %% 0. Upload image 
 % used double differences
-[bands, ~, REFMAT, ~] = uploadTiff(directory);
+[bands, CMAP, REFMAT, BBOX] = uploadTiff(directory);
+
+%% 0.1 Region of intrest
+coord.ZoomPalm = [ 1.284429739730507e+07,1.294529441923269e+07; ...
+                   6.863818069469999e+04,-3.427046674042370e+04 ];
+coord.ZoomForest = [ 1.284429739730507e+07,1.294529441923269e+07;
+                     6.863818069469999e+04,-3.427046674042370e+04] ;
+coord.ZoomDefor = [ 1.284429739730507e+07,1.294529441923269e+07; ...
+                    6.863818069469999e+04,-3.427046674042370e+04 ];
+
 
 %% 1. Histogram matching 
 
-Histogram_matching(bands, edit);
+% Histogram_matching(bands, edit);
 
 %% 2. Bands indices --> vendredi
 img = trueColor(bands);
@@ -19,9 +28,10 @@ img = doNDVI(bands, img);
 img = doNDMI(bands, img);
 
 %% Plot results
-plotIndices(img, REFMAT);
+%plotIndices(img, REFMAT);
 
 %% 3. Morphology 
+morphoAnalysis(bands, REFMAT, coord);
 
 %% 4. Difference Image Analysis
 
--- morphoAnalysis.m
+++ morphoAnalysis.m
@@ -1,14 +1,48 @@
-function morphoAnalysis()
+function [] = morphoAnalysis(bands, REFMAT, coord)
 
-    % Loop for every date
-    for t = 1:length(bands)
-        SE = strel('disk',5); % 'diamond' 'square'
+    % Extraction of all field names 
+    fn = fieldnames(bands); 
 
-        % Performing Erosion
-        Im_e = imerode(Im,SE);
-        % Performing Dilation
-        Im_d = imdilate(Im,SE);
+    % Preallocation of memory
+
+    % Loop for every "time"
+    for t = 1:length(bands) 
+        % Loop for bands (the first field is the date)
+        for b = 2:numel(fn)  
+
+            SE = strel('disk',5); % 'diamond' 'square'
+
+            % Performing Erosion
+            erosion(t).(fn{b}) = imerode(bands(t).(fn{b}),SE);
+            % Performing Dilation
+            dilation(t).(fn{b}) = imdilate(bands(t).(fn{b}),SE);
+
+            % Performing Opening
+            opening(t).(fn{b}) = imopen(bands(t).(fn{b}),SE);
+            % Performing Closing
+            closing(t).(fn{b}) = imclose(bands(t).(fn{b}),SE);
+
+        end
+
+    end 
+    
+    % =====================================================================
+    %   Plot of different graphic
+    % =====================================================================
+    % Loop for every "time"
+    for t = 1:length(bands) 
+        % Loop for bands (the first field is the date)
+        for b = 2:numel(fn) 
+            figure
+            H = mapshow(erosion(t).(fn{b}), REFMAT(t).(fn{b}));
+            ax = get(H, 'Parent');
+            set(ax, 'XLim', coord.ZoomPalm(1,:), 'YLim', coord.ZoomPalm(2,:))
+            xlabel('easting [m]')
+            ylabel('northing [m]')
+            title(['Morphological erosion', fn{b}]);
         
+        end
     end
+    
+end
 
-end 
